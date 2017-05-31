0d9a14afee76484d792db96246c183ece02d1852
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-05-31 14:51:54 +0200

    Skript RI_freqstatistics – Kommentare hinzugefügt.

--- RI_freqstatistics.m
+++ RI_freqstatistics.m
@@ -1,26 +1,26 @@
 cfg               = [];
-cfg.frequency     = [3 5];
-cfg.channel       = {'C3', 'Cz', 'C4'};
-cfg.parameter     = 'powspctrm';
-cfg.method        = 'stats';
-cfg.statistic     = 'paired-ttest';
-cfg.alpha         = 0.05;
-cfg.avgoverfreq   = 'yes';
-cfg.avgoverchan   = 'yes';
+cfg.frequency     = [3 5];                                                  % frequencies of interest
+cfg.channel       = {'C3', 'Cz', 'C4'};                                     % channels/components of interest
+cfg.parameter     = 'powspctrm';                                            % kind of data
+cfg.method        = 'stats';                                                % using MATLAB statistic toolbox for analysis  
+cfg.statistic     = 'paired-ttest';                                         % using depentend t-test
+cfg.alpha         = 0.05;                                                   % significance level   
+cfg.avgoverfreq   = 'yes';                                                  % average over frequencies (yes or no)
+cfg.avgoverchan   = 'yes';                                                  % average over channels (yes or no)
 
-data_fft_mean = cell(1,2);
+data_fft_mean = cell(1,2);                                                  % only for additional plotting purpose
 data_fft_mean{1} = data_hand_fft_mean;
 data_fft_mean{2} = data_head_fft_mean;
 
-hand_tmp = data_hand_fft;
+hand_tmp = data_hand_fft;                                                   % formatting steps, remove empty datasets from cell array
 head_tmp = data_head_fft;
 eHand = cellfun('isempty', hand_tmp);
 eHead = cellfun('isempty', head_tmp);
 hand_tmp(eHand) = [];
 head_tmp(eHead) = [];
 
-cfg.design=[ones(1, length(hand_tmp)), 2*ones(1, length(hand_tmp))];
+cfg.design=[ones(1, length(hand_tmp)), 2*ones(1, length(hand_tmp))];        % define design, sepearte the datasets into the two conditions
 
-stat = ft_freqstatistics(cfg, hand_tmp{:}, head_tmp{:});
+stat = ft_freqstatistics(cfg, hand_tmp{:}, head_tmp{:});                    % calculate statistics
 
-clear hand_tmp head_tmp eHand eHead
+clear hand_tmp head_tmp eHand eHead                                         % clear temporary variables
