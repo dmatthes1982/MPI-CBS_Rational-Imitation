b4110a04d86ba715a07db324b92df8524e871914
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-06-01 16:25:11 +0200

    Kleinigkeiten in RI_freqstatistics umformatiert, keine Funktionserweiterung

--- RI_freqstatistics.m
+++ RI_freqstatistics.m
@@ -1,12 +1,12 @@
 cfg               = [];
-cfg.frequency     = [3 5];                                                  % frequencies of interest
-cfg.channel       = {'C3', 'Cz', 'C4'};                                     % channels/components of interest
+cfg.frequency     = [0 1.4];                                                % frequencies of interest
+cfg.channel       = {'F4'};                                                 % channels/components of interest
 cfg.parameter     = 'powspctrm';                                            % kind of data
 cfg.method        = 'stats';                                                % using MATLAB statistic toolbox for analysis  
 cfg.statistic     = 'paired-ttest';                                         % using depentend t-test
 cfg.alpha         = 0.05;                                                   % significance level   
 cfg.avgoverfreq   = 'yes';                                                  % average over frequencies (yes or no)
-cfg.avgoverchan   = 'yes';                                                  % average over channels (yes or no)
+cfg.avgoverchan   = 'no';                                                   % average over channels (yes or no)
 
 data_fft_mean = cell(1,2);                                                  % only for additional plotting purpose
 data_fft_mean{1} = data_hand_fft_mean;
@@ -21,6 +21,6 @@ head_tmp(eHead) = [];
 
 cfg.design=[ones(1, length(hand_tmp)), 2*ones(1, length(hand_tmp))];        % define design, sepearte the datasets into the two conditions
 
-stat = ft_freqstatistics(cfg, hand_tmp{:}, head_tmp{:});                    % calculate statistics
+stat = ft_freqstatistics(cfg, head_tmp{:}, hand_tmp{:});                    % calculate statistics
 
 clear hand_tmp head_tmp eHand eHead                                         % clear temporary variables
