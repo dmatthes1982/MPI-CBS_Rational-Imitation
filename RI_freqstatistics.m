e9bfeaa1239f0ffdf69634cdf356b68cfa59aa8d
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-06-08 18:38:20 +0200

    Skript RI_freqstatistics  zur Funktion RI_freqstatistics(…) umgebaut. → Eingabeparameter cfg, data_hand und data_head → Über die Config kann cfg.frequency, cfg.channel, cfg.avgoverfreq, cfg.avgoverchan eingestellt werden.

--- RI_freqstatistics.m
+++ RI_freqstatistics.m
@@ -1,15 +1,29 @@
-cfg               = [];
-cfg.frequency     = [0 1.4];                                                % frequencies of interest
-cfg.channel       = {'F4'};                                                 % channels/components of interest
+function [data_pttest] = RI_freqstatistics(cfg, data_hand, data_head)
+% RI_FREQSTATISTICS conducts a paired-ttest between two conditions.
+%
+% Use as
+%   [data_pttest] = RI_freqstatistics(cfg, data_hand, data_head)
+% where the input data is the result from RI_PSDANALYSIS
+%
+% The configuration options are
+%    cfg.frequency    = number or range or 'all' (i.e. 6 or [beginn end]), unit = Hz     (default = 'all')
+%    cfg.channel      = 'all' or a specific selection (i.e. {'C3', 'P*', '*4'})          (default = 'all')
+%    cfg.avgoverfreq  = 'no' or 'yes'                                                    (default = 'no')
+%    cfg.avgoverchan  = 'no' or 'yes'                                                    (default = 'no')
+%
+% This function requires the fieldtrip toolbox.
+%
+% See also FT_FREQSTATISTICS
+
+% Copyright (C) 2017, Daniel Matthes, MPI CBS
+
 cfg.parameter     = 'powspctrm';                                            % kind of data
 cfg.method        = 'stats';                                                % using MATLAB statistic toolbox for analysis  
 cfg.statistic     = 'paired-ttest';                                         % using depentend t-test
 cfg.alpha         = 0.05;                                                   % significance level   
-cfg.avgoverfreq   = 'yes';                                                  % average over frequencies (yes or no)
-cfg.avgoverchan   = 'no';                                                   % average over channels (yes or no)
 
-hand_tmp = data_hand_fft;                                                   % formatting steps, remove empty datasets from cell array
-head_tmp = data_head_fft;
+hand_tmp = data_hand;                                                       % formatting steps, remove empty datasets from cell array
+head_tmp = data_head;
 eHand = cellfun('isempty', hand_tmp);
 eHead = cellfun('isempty', head_tmp);
 hand_tmp(eHand) = [];
@@ -17,6 +31,6 @@ head_tmp(eHead) = [];
 
 cfg.design=[ones(1, length(hand_tmp)), 2*ones(1, length(hand_tmp))];        % define design, sepearte the datasets into the two conditions
 
-stat = ft_freqstatistics(cfg, head_tmp{:}, hand_tmp{:});                    % calculate statistics
+data_pttest = ft_freqstatistics(cfg, head_tmp{:}, hand_tmp{:});             % calculate statistics
 
-clear hand_tmp head_tmp eHand eHead                                         % clear temporary variables
+end
