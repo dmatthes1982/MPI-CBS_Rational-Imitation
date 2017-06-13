58aeaf57cdec7c641cbba50d0222bed505b82e58
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-06-13 18:20:20 +0200

    Skript RI_mainTest – neues Skript → testet RI_redefinetrial, RI_psdanalysis, RI_psdPlot, RI_freqstatistics mit den Testdaten. Skript RI_genTestData – Fehler in der Erstellung der Testdaten behoben → Das Rauschen wird nun nicht nur einmalig, sondern stattdessen für jeden Trial neu generiert → Ansonsten erzeugt schon allein das unterschiedliche Rauschen in den unterschiedlichen Bedingungen Pseudo-Signifikanz.

--- RI_genTestData.m
+++ RI_genTestData.m
@@ -12,46 +12,31 @@ cfg.trllen  = trialLength./freqSamp;
 cfg.s1.freq = 2;
 cfg.s1.ampl = 400;
 cfg.s2.freq = 4;
-cfg.s2.ampl = 100;
+cfg.s2.ampl = 70;
 cfg.s3.freq = 10;
-cfg.s3.ampl = 50;
-cfg.noise.ampl = 20;
-
-test_data = ft_freqsimulation (cfg);
-sig_org_hand = test_data.trial{1}(1,:);
-
-test_data = ft_freqsimulation (cfg);
-sig_org_head = test_data.trial{1}(1,:);
-
-cfg.s2.ampl = 150;
-test_data = ft_freqsimulation (cfg);
-sig_theta = test_data.trial{1}(1,:);
-
-cfg.s2.ampl = 100;
-cfg.s3.ampl = 100;
-test_data = ft_freqsimulation (cfg);
-sig_alpha = test_data.trial{1}(1,:);
-
-clear freqSamp trialLength test_data cfg
+cfg.s3.ampl = 20;
+cfg.noise.ampl = 10;
+cfg.showcallinfo = 'no';
 
 for i=1:1:dataLength
+  fprintf('%02d\n', i);
   if ~isempty(data_hand{i})
     for j=1:1:trialsAveraged(i).hand
+      sig_orig = ft_freqsimulation(cfg);
       for k=1:1:channels
-        data_hand{i}.trial{j}(k,:) = sig_org_hand;
+        data_hand{i}.trial{j}(k,:) = sig_orig.trial{1}(1,:);
       end
     end
   end
   if ~isempty(data_head{i})
     for j=1:1:trialsAveraged(i).head
+      sig_orig = ft_freqsimulation(cfg);
       for k=1:1:channels
-        data_head{i}.trial{j}(k,:) = sig_org_head;
+        data_head{i}.trial{j}(k,:) = sig_orig.trial{1}(1,:);
       end
     end    
   end  
 end
-
-clear i j k sig_org_hand sig_org_head
 
 labelFrontal = {'F3' 'F4' 'Fz'};
 labelCentral = {'C3' 'C4' 'Cz'};
@@ -63,23 +48,30 @@ for i=1:1:3
   posCentral(i) = find(strcmp(data_head{1}.label, labelCentral{i}));
 end
 
-clear labelFrontal labelCentral i
-
 for i=1:1:dataLength
+  fprintf('%02d\n', i);
+  cfg.s2.ampl = 120;
+  cfg.s3.ampl = 20;
   if ~isempty(data_hand{i})
-    for j=1:1:trialsAveraged(i).hand
+    for j=1:1:trialsAveraged(i).head
+      sig_theta = ft_freqsimulation (cfg);
       for k=posFrontal
-        data_head{i}.trial{j}(k,:) = sig_theta;
+        data_head{i}.trial{j}(k,:) = sig_theta.trial{1}(1,:);
       end
     end
   end
+  cfg.s2.ampl = 70;
+  cfg.s3.ampl = 60;
   if ~isempty(data_head{i})
     for j=1:1:trialsAveraged(i).head
+      sig_alpha = ft_freqsimulation (cfg);
       for k=posCentral
-        data_head{i}.trial{j}(k,:) = sig_alpha;
+        data_head{i}.trial{j}(k,:) = sig_alpha.trial{1}(1,:);
       end
     end    
   end  
 end
 
-clear i j k sig_alpha sig_theta posFrontal posCentral channels dataLength
+clear i j k sig_alpha sig_theta posFrontal posCentral channels ...
+      dataLength sig_theta sig_alpha freqSamp trialLength labelFrontal ...
+      labelCentral cfg sig_orig
---
+++ RI_mainTest.m
@@ -0,0 +1,74 @@
+%% load or generate data
+
+load('../../test/RationalImitation/RI_test_01_Preprocessed.mat');
+%RI_genTestData;
+
+%% shorten trials
+data_head_short = RI_redefinetrial(data_head);
+data_hand_short = RI_redefinetrial(data_hand);
+
+%% calculate PSD
+data_head_fft = RI_psdanalysis(data_head_short);
+data_hand_fft = RI_psdanalysis(data_hand_short);
+
+%% calculate mean PSD over all people
+RI_averagePeople;
+
+%% create and export plots
+fig_01_title = 'Hand - Mean power of every person';
+fig_02_title = 'Head - Mean power of every person';
+fig_03_title = 'Hand - Power over all persons';
+fig_04_title = 'Head - Power over all persons';
+fig_05_title = 'Hand vs. Head - Power over all persons';
+pdf_01_title = '../../test/RationalImitation/Hand';
+pdf_02_title = '../../test/RationalImitation/Head';
+pdf_03_title = '../../test/RationalImitation/HandMean';
+pdf_04_title = '../../test/RationalImitation/HeadMean';
+pdf_05_title = '../../test/RationalImitation/Compare-HandHeadMean';
+
+RI_psdPlot(data_hand_fft, fig_01_title, pdf_01_title);
+RI_psdPlot(data_head_fft, fig_02_title, pdf_02_title);
+RI_psdPlot(data_hand_fft_mean, fig_03_title, pdf_03_title);
+RI_psdPlot(data_head_fft_mean, fig_04_title, pdf_04_title);
+
+data_fft_mean = cell(1,2);
+data_fft_mean{1} = data_hand_fft_mean;
+data_fft_mean{2} = data_head_fft_mean;
+
+RI_psdPlot(data_fft_mean, fig_05_title, pdf_05_title);
+
+%% clear temporary variables
+clear fig_01_title fig_02_title fig_03_title fig_04_title fig_05_title ...
+      pdf_01_title pdf_02_title pdf_03_title pdf_04_title pdf_05_title ...
+      data_fft_mean
+
+%% calculate paired t-test
+
+cfg = [];
+cfg.frequency = 10;
+cfg.channel = 'C4';
+
+ptttest_10Hz_C4 = RI_freqstatistics(cfg, data_hand_fft, data_head_fft);
+
+cfg.frequency = 10;
+cfg.channel = 'F3';
+
+ptttest_10Hz_F3 = RI_freqstatistics(cfg, data_hand_fft, data_head_fft);
+
+cfg.frequency = 4;
+cfg.channel = 'C4';
+
+ptttest_4Hz_C4 = RI_freqstatistics(cfg, data_hand_fft, data_head_fft);
+
+cfg.frequency = 4;
+cfg.channel = 'F3';
+
+ptttest_4Hz_F3 = RI_freqstatistics(cfg, data_hand_fft, data_head_fft);
+
+clear cfg;
+
+
+
+
+
+
