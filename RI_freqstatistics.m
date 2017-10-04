50e7208704c314f5060444beb66dfd941c36d60e
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-10-04 12:01:49 +0200

    Funktion RI_freqstatistics(…) – Begonnen die Funktion zu modifizieren, dass einerseits md  und sd auch richtig berechnet wird, wenn avgoverchan = yes ist und das zudem eine Tabelle mit den Werten/Mittwelwerten der einzelnen Dyaden generiert wird (zur Verifikation mit SPSS). Funktion RI_freqstatistics(…) – Modifikation der Funktion erfolgreich abgeschlossen, cfg.avgoverfreq ist nun default-mäßig yes.

--- RI_freqstatistics.m
+++ RI_freqstatistics.m
@@ -1,4 +1,4 @@
-function [data_pttest] = RI_freqstatistics(cfg, data_hand, data_head)
+function [data_pttest, pttestSource] = RI_freqstatistics(cfg, data_hand, data_head)
 % RI_FREQSTATISTICS conducts a paired-ttest between two conditions.
 %
 % Use as
@@ -8,7 +8,6 @@ function [data_pttest] = RI_freqstatistics(cfg, data_hand, data_head)
 % The configuration options are
 %    cfg.frequency    = number or range or 'all' (i.e. 6 or [begin end]), unit = Hz     (default = 'all')
 %    cfg.channel      = 'all' or a specific selection (i.e. {'C3', 'P*', '*4'})         (default = 'all')
-%    cfg.avgoverfreq  = 'no' or 'yes'                                                   (default = 'no')
 %    cfg.avgoverchan  = 'no' or 'yes'                                                   (default = 'no')
 %
 % This function requires the fieldtrip toolbox.
@@ -20,7 +19,10 @@ function [data_pttest] = RI_freqstatistics(cfg, data_hand, data_head)
 % -------------------------------------------------------------------------
 % Calculate paired t-test
 % -------------------------------------------------------------------------
+cfg.avgoverchan = ft_getopt(cfg, 'avgoverchan', 'no');
+
 cfg.parameter     = 'powspctrm';                                            % kind of data
+cfg.avgoverfreq   = 'yes';
 cfg.method        = 'stats';                                                % using MATLAB statistic toolbox for analysis  
 cfg.statistic     = 'paired-ttest';                                         % using depentend t-test
 cfg.alpha         = 0.05;                                                   % significance level   
@@ -62,7 +64,12 @@ else
   [channel, chnNum] = RI_channelselection(channel, data_hand{num}.label);
 end
 
-numOfElec = length(channel);                                                % Get number of channels
+numOfChan = length(channel);                                                % Get number of selected channels
+if strcmp(cfg.avgoverchan, 'yes')                                           % Get number of actual channels /components (depends on cfg.avgoverchan)
+  numOfComp = 1;
+else
+  numOfComp = numOfChan;
+end
 
 % -------------------------------------------------------------------------
 % Determine frequencies of interest
@@ -96,46 +103,83 @@ if(length(freq) == 2)                                                       % Ca
   end
 end
 
-
 % -------------------------------------------------------------------------
 % Create reduced power spectrum relating to channels of interest
 % -------------------------------------------------------------------------
 powspctrmHead{length(data_head)} = [];
 powspctrmHand{length(data_head)} = [];
 
-for i=1:1:numOfElec
+for i=1:1:numOfChan
   for j=1:1:length(data_head)
     if ~isempty(data_head{j})
-      powspctrmHead{j}(i,:) = mean(data_head{1, j}.powspctrm(chnNum{i},:), 1);
-      powspctrmHand{j}(i,:) = mean(data_hand{1, j}.powspctrm(chnNum{i},:), 1);
+      powspctrmHead{j}(i,:) = data_head{1, j}.powspctrm(chnNum{i},:);
+      powspctrmHand{j}(i,:) = data_hand{1, j}.powspctrm(chnNum{i},:);
     end
   end
 end
 
 % -------------------------------------------------------------------------
-% Calculate descriptive statistic
+% Calculate descriptive statistic over all dyads
+% Generate pttest source table (for comparision with SPSS)
 % -------------------------------------------------------------------------
-chanNames{2*numOfElec} = [];
-for i=1:1:numOfElec
-  chanNames{i}            = strcat(channel{i}, 'Head');
-  chanNames{i+numOfElec}  = strcat(channel{i}, 'Hand');
+chanNames{2*numOfComp+1} = [];
+chanNames{1} = 'participant';
+
+if strcmp(cfg.avgoverchan, 'yes')
+  chanNames{2} = [];
+  chanNames{3} = [];
+  if numOfChan > 1
+    for i = 1:1:numOfChan
+      chanNames{2} = [chanNames{2}, channel{i}];
+      chanNames{3} = [chanNames{3}, channel{i}];
+    end
+  end
+  chanNames{2} = [chanNames{2}, 'Head'];
+  chanNames{3} = [chanNames{3}, 'Hand'];
+else
+  for i = 1:1:numOfChan
+    chanNames{i+1}            = [channel{i}, 'Head'];
+    chanNames{i+1+numOfComp}  = [channel{i}, 'Hand'];
+  end
 end
 
-descStat = array2table(zeros(2, 2*numOfElec), 'VariableNames', chanNames);  % Generate descriptive statistics table
+descStat = array2table(zeros(2, 2*numOfComp), 'VariableNames', ...          % Generate descriptive statistics table template
+                        chanNames(2:end));  
 descStat.Properties.RowNames = {'mean', 'standard deviation'};
 
-matrixHead = cat(3, powspctrmHead{:});
+numOfPart = find(~cellfun(@isempty, data_hand));
+pttestSource = array2table(zeros(length(numOfPart), 2*numOfComp+1), ...     % Generate pttestSource table template
+                          'VariableNames', chanNames);
+pttestSource.participant = numOfPart';
+
+matrixHead = cat(3, powspctrmHead{:});                                      % Convert data matrix from cell to array structure
 matrixHand = cat(3, powspctrmHand{:});
 
-for i=1:1:numOfElec
+if strcmp(cfg.avgoverchan, 'yes')
+  matrixHead = mean(matrixHead, 1);
+  matrixHand = mean(matrixHand, 1);
+end
+
+for i=1:1:numOfComp                                                         % Insert data into descriptive statistics table
   descStat(1,i) = num2cell(mean(mean(matrixHead(i,freqCols,:),2),3));
   descStat(2,i) = num2cell(std(mean(matrixHead(i,freqCols,:),2),0,3));
-  descStat(1,i+numOfElec) = num2cell(mean(mean(matrixHand(i, ...
+  descStat(1,i+numOfComp) = num2cell(mean(mean(matrixHand(i, ...
                                   freqCols,:),2),3));
-  descStat(2,i+numOfElec) = num2cell(std(mean(matrixHand(i, ...
+  descStat(2,i+numOfComp) = num2cell(std(mean(matrixHand(i, ...
                                   freqCols,:),2),0,3));
 end
 
 data_pttest.descStat = descStat;
+
+for i=1:1:numOfComp
+  for j=1:1:(length(numOfPart))
+    pttestSource(j, i+1)            = num2cell(mean(...
+                                      matrixHead(i,freqCols,j), 2));
+    pttestSource(j, i+1+numOfComp)  = num2cell(mean(...
+                                      matrixHand(i,freqCols,j), 2));
+  end
+end
+
+data_pttest.pttestSource = pttestSource;
 
 end
